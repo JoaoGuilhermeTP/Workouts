eae8eaf4cdae960158237e429a18d421228790ac
João Guilherme <adm.joaoguilherme@gmail.com>
2024-10-20 11:36:25 -0300

    Capitalized exercise list using styles istead of toUpperCase function

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,28 +1,32 @@
 import { StyleSheet, Text, View, FlatList } from "react-native";
 
-export default function ExerciseListItem({item}) {
-    console.log({item});
-    return (
-      <View style={styles.exerciseContainer}>
-        <Text style={styles.exerciseName}>{item.name}</Text>
-        <Text style={styles.exerciseSubtitle}>
-          {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
-        </Text>
-      </View>
-    );
-  }
+export default function ExerciseListItem({ item }) {
+  console.log({ item });
+  return (
+    <View style={styles.exerciseContainer}>
+      <Text style={styles.exerciseName}>{item.name}</Text>
+      <Text style={styles.exerciseSubtitle}>
+        <Text style={styles.subItem}>{item.muscle} | </Text>
+        <Text style={styles.subItem}>{item.equipment} </Text>
+      </Text>
+    </View>
+  );
+}
 
-  const styles = StyleSheet.create({
-    exerciseContainer: {
-      backgroundColor: "#fff",
-      padding: 10,
-      borderRadius: 10,
-    },
-    exerciseName: {
-      fontSize: 20,
-      fontWeight: "500",
-    },
-    exerciseSubtitle: {
-      color: "dimgray",
-    },
-  });
+const styles = StyleSheet.create({
+  exerciseContainer: {
+    backgroundColor: "#fff",
+    padding: 10,
+    borderRadius: 10,
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  exerciseSubtitle: {
+    color: "dimgray",
+  },
+  subItem: {
+    textTransform: "capitalize",
+  },
+});
